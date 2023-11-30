de3f1fcf447cd32b8e0939bc6d5fb3809b5ad089
Drobyshev Vladimir <map.heldar@yandex.ru>
2023-11-30 13:32:37 +0300

    Microcharts

--- AvaloniaChartsComparison/AvaloniaChartsComparison/ViewModels/MicrochartsViewModel.cs
+++ AvaloniaChartsComparison/AvaloniaChartsComparison/ViewModels/MicrochartsViewModel.cs
@@ -1,10 +1,15 @@
-﻿using AvaloniaChartsComparison.Models;
+﻿using System.Linq;
+using Avalonia.Microcharts;
+using AvaloniaChartsComparison.Models;
 
 namespace AvaloniaChartsComparison.ViewModels;
 
 public class MicrochartsViewModel : ChartViewModelBase
 {
+    public LineChart Chart { get; }
+
     public MicrochartsViewModel(DataGenerator dataGenerator) : base(dataGenerator)
     {
+        Chart = new LineChart { Entries = dataGenerator.SmallXYSeriesValues[0].Select(d => new Entry { Value = (float)d }) };
     }
 }
